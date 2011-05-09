5f0d22a46c1f0a5cba1b4ab037c7654f4ca28473
lee212 <lee212@indiana.edu>
2011-05-09 21:33:08 +0000

    patch updated (using diff) with a script (version info included)

--- patch/FG-1087/apply.2.0.2.or.higher.sh
+++ patch/FG-1087/apply.2.0.2.or.higher.sh
@@ -41,22 +41,27 @@ fi
 
 ########################################################################
 #3. Apply patch
-PATCH_FILENAME="EucalyptusWebInterface.java"
-PATCH_URL="https://129.79.49.76:8443/"
+PATCH_FILENAME="fg-1087.patch"
+ORI_NAME="EucalyptusWebInterface.java"
+tmp="FG-1087"
+PATCH_URL="https://futuregrid.svn.sourceforge.net/svnroot/futuregrid/core/Eucalyptus/patch/FG-1087/ $tmp"
 PATCH_PATH="eucalyptus-$EUCA_VER/clc/modules/www/src/main/java/edu/ucsb/eucalyptus/admin/client/"
 PATCH_BUILD_PATH="eucalyptus-$EUCA_VER/clc/modules/www"
 
 #1) download
 echo "\nDownloading patch files..."
-/usr/bin/wget --no-check-certificate -c $PATCH_URL$PATCH_FILENAME
+#/usr/bin/wget --no-check-certificate -c $PATCH_URL$PATCH_FILENAME
+svn co $PATCH_URL
 if [ $? -ne 0 ]
 then
+	echo "No svn?\n"
 	exit -1
 fi
 
 echo "\nCopying patch files..."
 #2) copy into source directory
-cp -i $PATCH_FILENAME $DIRE"/"$PATCH_PATH"/"$PATCH_FILENAME
+#cp -i $PATCH_FILENAME $DIRE"/"$PATCH_PATH"/"$PATCH_FILENAME
+/usr/bin/patch $DIRE"/"$PATCH_PATH"/"$ORI_NAME < $tmp/$PATCH_FILENAME
 
 echo "\nIn order to generate root.war on eucalyptus-$EUCA_VER/clc/modules/www, installation of eucalyptus need to be done first. Read INSTALL."
 ########################################################################

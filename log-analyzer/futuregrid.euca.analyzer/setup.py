f62247b1d10c6f88a309994f38974fbdbe784678
Gregor von Laszewski <laszewski@gmail.com>
2012-02-10 11:53:40 -0500

    here a sample generated with paster for an egg python distribution

---
+++ log-analyzer/futuregrid.euca.analyzer/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='futuregrid.euca.analyzer',
+      version=version,
+      description="The package allows the analysis of Eucalyptus logs and display the information graphically",
+      long_description="""\
+The package allows the analysis of Eucalyptus logs and display the information graphically""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='FutureGrid Eucalyptys Log File Analysis',
+      author='Gregor von Laszewski',
+      author_email='laszewski@gmail.com',
+      url='http://futuregrid.org',
+      license='Apache 2.0',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )

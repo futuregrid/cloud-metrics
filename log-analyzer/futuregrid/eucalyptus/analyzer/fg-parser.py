f1d75112981a370335c376d0ee3d977a15761a64
Gregor von Laszewski <laszewski@gmail.com>
2012-02-12 12:26:52 -0500

    first example of a simplified instance parser

--- log-analyzer/futuregrid/eucalyptus/analyzer/fg-parser.py
+++ log-analyzer/futuregrid/eucalyptus/analyzer/fg-parser.py
@@ -1,15 +1,28 @@
 #! /usr/bin/env python
 import re
+import json
+import sys
+
+def convert_str_to_dict_str(line):
+    line = re.sub(' +',' ',line)
+    line = line.strip(" ")
+    line = re.sub(' ',',',line)
+
+    # more regular dict
+    line = re.sub('=','\'=\'',line)
+    line = re.sub(',','\',\'',line)
+    line = re.sub('=',' : ',line)
+    return '{\'' + line + '\'}'
 
 def print_ccInstance_parser(line):
     """parses the line and returns a dict"""
     data = {}
     data['test'] = "Hallo world"
 
-    # split line after the third ]
-    
-    m = re.search( r'\[(.*)\]\[(.*)\]\[(.*)\](.*)', line, re.M|re.I)
+    # split line after the third ] to (find date, id, msgtype)
+    # put the rest in the string "rest"
 
+    m = re.search( r'\[(.*)\]\[(.*)\]\[(.*)\](.*)', line, re.M|re.I)
     data['date'] = m.group(1)
     data['id']   = m.group(2)
     data['msgtype'] = m.group(3)
@@ -18,44 +31,25 @@ def print_ccInstance_parser(line):
     rest = rest.replace("print_ccInstance():","linetype=print_ccInstance")
     # replace refreshinstances(): with calltype=refresh_instances
     rest = rest.replace("refresh_instances():","calltype=refresh_instances")   # removing multiple spaces
-    rest = re.sub(' +',' ',rest)
-    rest = rest.strip(" ")
-    # replace " }" with "}"
     rest = re.sub(' +}','}',rest)
-    # fix empty assignments with "= "
-    rest = re.sub('= +','={} ',rest)
-    # introducing commas
-    rest = re.sub(' ',',',rest)
-
-    # more regular dict
-    rest = re.sub('=','\'=\'',rest)
-    rest = re.sub(',','\',\'',rest)
-    rest = re.sub('=',' : ',rest)
 
 
-    # adding dict
-    rest = "{'"+rest+"'}"
-    print "----"
-    print "REST>" + rest
-    print "----"
+    #separate easy assignments from those that would contain groups, for now simply put groups as a string
+    # all others are merged into a string with *=* into rest
+    m = re.search( r'(.*)keyName=(.*)ccnet=(.*)ccvm=(.*)ncHostIdx=(.*)volumes=(.*)groupNames=(.*)', rest, re.M|re.I)
+    data['keyName'] = m.group(2).strip()
+    data["ccnet"] = m.group(3).strip()
+    data["ccvm"] = m.group(4).strip()
+    data["volumes"] = m.group(6).strip()
+    data["groupNames"] = m.group(7).strip()
+    # assemble the rest string
+    rest = m.group(1) + "ncHostIdx=" +m.group(5)
 
-    data2 = eval(rest)
+    # GATHER ALL SIMPLE *=* assignments into a single rest line and add each entry to dict via eval
+    rest = convert_str_to_dict_str(rest)
+    restdata = eval (rest)
 
-    print data2
-
-#    mm = re.search( r'(.*)=(.*)|\{(.*)\},', rest, re.M|re.I)
-#    mm = re.search( r'(.*)=[(.*)|\{(.*)\}],', rest, re.M|re.I)
-
-#    print mm.groups()
-#    print "GROUP1>" 
-#    print mm.groups(1)
-#    print "GROUP1>" 
-#    print mm.groups(2)
-
-    # replace " " with ,
-    # put { } around 
-    # assign to the data via eval? not sure if things such as ccvm={ } would work, also groupnames and volumes are probably arrays 
-    # this intermediate structure may be easier to parse
+    data.update(restdata)
 
     return data
 
@@ -65,18 +59,12 @@ def print_ccInstance_parser(line):
 #####################################################################
 def main():
 
-    s = "{'linetype' : 'print_ccInstance','calltype' : 'refresh_instances','instanceId' : 'i-42BA06B1'}"    
+    line = "[Wed Nov  9 19:58:12 2011][008128][EUCADEBUG ] print_ccInstance(): refresh_instances():  instanceId=i-42BA06B1 reservationId=r-3D3306BC emiId=emi-0B951139 kernelId=eki-78EF12D2 ramdiskId=eri-5BB61255 emiURL=http://149.165.146.135:8773/services/Walrus/centos53/centos.5-3.x86-64.img.manifest.xml kernelURL=http://149.165.146.135:8773/services/Walrus/xenkernel/vmlinuz-2.6.27.21-0.1-xen.manifest.xml ramdiskURL=http://149.165.146.135:8773/services/Walrus/xeninitrd/initrd-2.6.27.21-0.1-xen.manifest.xml state=Extant ts=1320693195 ownerId=sharif keyName=ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCp13CbKJLtKG5prGiet/VHct36CXzcpBKVgYsh/lxIXWKuositayvvuKux+I5GZ9bFWzMF71xAjmFinmAT3FXFKMd54FebPKZ2kBPCRqtmxz2jT1SG4hy1g1eDPzVX+qt5w8metAs7W//BCaBvWpU5IBuKSNqxO5OUIjIKkw3xkSswRpqSzrUBAmQP7e4dzQvmhqIxq4ZWqcEIWsAik0fSODTipa+Z6DvVKe02f5OtdUsXzz7pIivZ3qRGQveI5SOTdgFPqG+VglMsPURLbFFVWW1l51gCmRUwTf9ClySshSpkpAtaOx/OApQoII/vJxgr/EdYPOu1QLkubS4XH6+Z sharif@eucalyptus ccnet={privateIp=10.0.4.4 publicIp=149.165.159.130 privateMac=D0:0D:42:BA:06:B1 vlan=10 networkIndex=4} ccvm={cores=1 mem=512 disk=5} ncHostIdx=24 serviceTag=http://i1:8775/axis2/services/EucalyptusNC userData= launchIndex=0 volumesSize=0 volumes={} groupNames={sharifnew }"
 
-    d1 = eval (s)
-    print d1
-    print d1['linetype']
+    print line
+    data = print_ccInstance_parser(line)
 
-    exit
-
-#    line = "[Wed Nov  9 19:58:12 2011][008128][EUCADEBUG ] print_ccInstance(): refresh_instances():  instanceId=i-42BA06B1 reservationId=r-3D3306BC emiId=emi-0B951139 kernelId=eki-78EF12D2 ramdiskId=eri-5BB61255 emiURL=http://149.165.146.135:8773/services/Walrus/centos53/centos.5-3.x86-64.img.manifest.xml kernelURL=http://149.165.146.135:8773/services/Walrus/xenkernel/vmlinuz-2.6.27.21-0.1-xen.manifest.xml ramdiskURL=http://149.165.146.135:8773/services/Walrus/xeninitrd/initrd-2.6.27.21-0.1-xen.manifest.xml state=Extant ts=1320693195 ownerId=sharif keyName=ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCp13CbKJLtKG5prGiet/VHct36CXzcpBKVgYsh/lxIXWKuositayvvuKux+I5GZ9bFWzMF71xAjmFinmAT3FXFKMd54FebPKZ2kBPCRqtmxz2jT1SG4hy1g1eDPzVX+qt5w8metAs7W//BCaBvWpU5IBuKSNqxO5OUIjIKkw3xkSswRpqSzrUBAmQP7e4dzQvmhqIxq4ZWqcEIWsAik0fSODTipa+Z6DvVKe02f5OtdUsXzz7pIivZ3qRGQveI5SOTdgFPqG+VglMsPURLbFFVWW1l51gCmRUwTf9ClySshSpkpAtaOx/OApQoII/vJxgr/EdYPOu1QLkubS4XH6+Z sharif@eucalyptus ccnet={privateIp=10.0.4.4 publicIp=149.165.159.130 privateMac=D0:0D:42:BA:06:B1 vlan=10 networkIndex=4} ccvm={cores=1 mem=512 disk=5} ncHostIdx=24 serviceTag=http://i1:8775/axis2/services/EucalyptusNC userData= launchIndex=0 volumesSize=0 volumes={} groupNames={sharifnew }"
-
-#    print line
-#    print print_ccInstance_parser(line)
+    print json.dumps(data, sort_keys=False, indent=4)
 
 if __name__ == "__main__":
     main()

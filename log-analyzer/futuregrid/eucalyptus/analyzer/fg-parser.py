5ec602bf484567346731e22846c58117eb218d1e
Gregor von Laszewski <laszewski@gmail.com>
2012-02-12 10:54:10 -0500

    improving the simple parser, starting to use eval

--- log-analyzer/futuregrid/eucalyptus/analyzer/fg-parser.py
+++ log-analyzer/futuregrid/eucalyptus/analyzer/fg-parser.py
@@ -19,14 +19,39 @@ def print_ccInstance_parser(line):
     # replace refreshinstances(): with calltype=refresh_instances
     rest = rest.replace("refresh_instances():","calltype=refresh_instances")   # removing multiple spaces
     rest = re.sub(' +',' ',rest)
+    rest = rest.strip(" ")
     # replace " }" with "}"
     rest = re.sub(' +}','}',rest)
     # fix empty assignments with "= "
     rest = re.sub('= +','={} ',rest)
-    
+    # introducing commas
+    rest = re.sub(' ',',',rest)
+
+    # more regular dict
+    rest = re.sub('=','\'=\'',rest)
+    rest = re.sub(',','\',\'',rest)
+    rest = re.sub('=',' : ',rest)
+
+
+    # adding dict
+    rest = "{'"+rest+"'}"
     print "----"
-    print "RRRR" + rest
+    print "REST>" + rest
     print "----"
+
+    data2 = eval(rest)
+
+    print data2
+
+#    mm = re.search( r'(.*)=(.*)|\{(.*)\},', rest, re.M|re.I)
+#    mm = re.search( r'(.*)=[(.*)|\{(.*)\}],', rest, re.M|re.I)
+
+#    print mm.groups()
+#    print "GROUP1>" 
+#    print mm.groups(1)
+#    print "GROUP1>" 
+#    print mm.groups(2)
+
     # replace " " with ,
     # put { } around 
     # assign to the data via eval? not sure if things such as ccvm={ } would work, also groupnames and volumes are probably arrays 
@@ -39,10 +64,19 @@ def print_ccInstance_parser(line):
 # MAIN
 #####################################################################
 def main():
-    line = "[Wed Nov  9 19:58:12 2011][008128][EUCADEBUG ] print_ccInstance(): refresh_instances():  instanceId=i-42BA06B1 reservationId=r-3D3306BC emiId=emi-0B951139 kernelId=eki-78EF12D2 ramdiskId=eri-5BB61255 emiURL=http://149.165.146.135:8773/services/Walrus/centos53/centos.5-3.x86-64.img.manifest.xml kernelURL=http://149.165.146.135:8773/services/Walrus/xenkernel/vmlinuz-2.6.27.21-0.1-xen.manifest.xml ramdiskURL=http://149.165.146.135:8773/services/Walrus/xeninitrd/initrd-2.6.27.21-0.1-xen.manifest.xml state=Extant ts=1320693195 ownerId=sharif keyName=ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCp13CbKJLtKG5prGiet/VHct36CXzcpBKVgYsh/lxIXWKuositayvvuKux+I5GZ9bFWzMF71xAjmFinmAT3FXFKMd54FebPKZ2kBPCRqtmxz2jT1SG4hy1g1eDPzVX+qt5w8metAs7W//BCaBvWpU5IBuKSNqxO5OUIjIKkw3xkSswRpqSzrUBAmQP7e4dzQvmhqIxq4ZWqcEIWsAik0fSODTipa+Z6DvVKe02f5OtdUsXzz7pIivZ3qRGQveI5SOTdgFPqG+VglMsPURLbFFVWW1l51gCmRUwTf9ClySshSpkpAtaOx/OApQoII/vJxgr/EdYPOu1QLkubS4XH6+Z sharif@eucalyptus ccnet={privateIp=10.0.4.4 publicIp=149.165.159.130 privateMac=D0:0D:42:BA:06:B1 vlan=10 networkIndex=4} ccvm={cores=1 mem=512 disk=5} ncHostIdx=24 serviceTag=http://i1:8775/axis2/services/EucalyptusNC userData= launchIndex=0 volumesSize=0 volumes={} groupNames={sharifnew }"
 
-    print line
-    print print_ccInstance_parser(line)
+    s = "{'linetype' : 'print_ccInstance','calltype' : 'refresh_instances','instanceId' : 'i-42BA06B1'}"    
+
+    d1 = eval (s)
+    print d1
+    print d1['linetype']
+
+    exit
+
+#    line = "[Wed Nov  9 19:58:12 2011][008128][EUCADEBUG ] print_ccInstance(): refresh_instances():  instanceId=i-42BA06B1 reservationId=r-3D3306BC emiId=emi-0B951139 kernelId=eki-78EF12D2 ramdiskId=eri-5BB61255 emiURL=http://149.165.146.135:8773/services/Walrus/centos53/centos.5-3.x86-64.img.manifest.xml kernelURL=http://149.165.146.135:8773/services/Walrus/xenkernel/vmlinuz-2.6.27.21-0.1-xen.manifest.xml ramdiskURL=http://149.165.146.135:8773/services/Walrus/xeninitrd/initrd-2.6.27.21-0.1-xen.manifest.xml state=Extant ts=1320693195 ownerId=sharif keyName=ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCp13CbKJLtKG5prGiet/VHct36CXzcpBKVgYsh/lxIXWKuositayvvuKux+I5GZ9bFWzMF71xAjmFinmAT3FXFKMd54FebPKZ2kBPCRqtmxz2jT1SG4hy1g1eDPzVX+qt5w8metAs7W//BCaBvWpU5IBuKSNqxO5OUIjIKkw3xkSswRpqSzrUBAmQP7e4dzQvmhqIxq4ZWqcEIWsAik0fSODTipa+Z6DvVKe02f5OtdUsXzz7pIivZ3qRGQveI5SOTdgFPqG+VglMsPURLbFFVWW1l51gCmRUwTf9ClySshSpkpAtaOx/OApQoII/vJxgr/EdYPOu1QLkubS4XH6+Z sharif@eucalyptus ccnet={privateIp=10.0.4.4 publicIp=149.165.159.130 privateMac=D0:0D:42:BA:06:B1 vlan=10 networkIndex=4} ccvm={cores=1 mem=512 disk=5} ncHostIdx=24 serviceTag=http://i1:8775/axis2/services/EucalyptusNC userData= launchIndex=0 volumesSize=0 volumes={} groupNames={sharifnew }"
+
+#    print line
+#    print print_ccInstance_parser(line)
 
 if __name__ == "__main__":
     main()

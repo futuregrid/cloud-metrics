c3089394a53a396d86b4b9af96e2181976770ded
Gregor von Laszewski <laszewski@gmail.com>
2012-02-12 16:42:34 -0500

    added working refresh resource parser

--- log-analyzer/futuregrid/eucalyptus/analyzer/fg-parser.py
+++ log-analyzer/futuregrid/eucalyptus/analyzer/fg-parser.py
@@ -28,25 +28,29 @@ def convert_str_to_dict_str(line):
     line = re.sub('=',' : ',line)
     return '{\'' + line + '\'}'
 
-def print_ccInstance_parser(line):
-    """parses the line and returns a dict"""
-    data = {}
-    data['test'] = "Hallo world"
-
+def parse_type_and_date(line,data):
     # split line after the third ] to (find date, id, msgtype)
     # put the rest in the string "rest"
-
     m = re.search( r'\[(.*)\]\[(.*)\]\[(.*)\](.*)', line, re.M|re.I)
-    data['date'] = m.group(1)
+    data['date'] = str(datetime.strptime(m.group(1), '%a %b %d %H:%M:%S %Y'))
     data['id']   = m.group(2)
     data['msgtype'] = m.group(3)
     rest =  m.group(4)
+    rest = re.sub(' +}','}',rest)
+    return rest
+
+
+def ccInstance_parser(line):
+    """parses the line and returns a dict"""
+    data = {}
+
+    # parse_type_and_date(line,data,rest)
+    rest = parse_type_and_date (line, data)
+
     # replace print_ccInstance(): with linetype=print_ccInstance
     rest = rest.replace("print_ccInstance():","linetype=print_ccInstance")
     # replace refreshinstances(): with calltype=refresh_instances
     rest = rest.replace("refresh_instances():","calltype=refresh_instances")   # removing multiple spaces
-    rest = re.sub(' +}','}',rest)
-
 
     #separate easy assignments from those that would contain groups, for now simply put groups as a string
     # all others are merged into a string with *=* into rest
@@ -72,12 +76,25 @@ def print_ccInstance_parser(line):
     convert_data_to_list(data,"groupNames")
     convert_data_to_list(data,"volumes")
 
-    #converting date
-    date_s = data["date"] 
-    data["date"] = str(datetime.strptime(date_s, '%a %b %d %H:%M:%S %Y'))
-
     return data
 
+def refresh_resource_parser(line):
+    line = "[Wed Nov  9 19:50:08 2011][008128][EUCADEBUG ] refresh_resources(): received data from node=i2 mem=24276/22740 disk=306400/305364 cores=8/6"
+    data = {}
+    data["calltype"] = "refresh_resources" 
+    rest = parse_type_and_date(line,data)
+    rest = re.sub("refresh_resources\(\): received data from","",rest).strip()
+    # node=i2 mem=24276/22740 disk=306400/305364 cores=8/6
+    m = re.search( r'node=(.*) mem=(.*)[/](.*) disk=(.*)/(.*) cores=(.*)/(.*)', rest, re.M|re.I)
+    data["node"] = m.group(1)
+    data["mem"] = m.group(2)
+    data["mem_max"] = m.group(3)
+    data["disk"] = m.group(4)
+    data["disk_max"] = m.group(5)
+    data["cores"] = m.group(6)
+    data["cores_max"] = m.group(7)
+
+    return data
 
 #####################################################################
 # MAIN
@@ -87,8 +104,12 @@ def main():
     line = "[Wed Nov  9 19:58:12 2011][008128][EUCADEBUG ] print_ccInstance(): refresh_instances():  instanceId=i-42BA06B1 reservationId=r-3D3306BC emiId=emi-0B951139 kernelId=eki-78EF12D2 ramdiskId=eri-5BB61255 emiURL=http://149.165.146.135:8773/services/Walrus/centos53/centos.5-3.x86-64.img.manifest.xml kernelURL=http://149.165.146.135:8773/services/Walrus/xenkernel/vmlinuz-2.6.27.21-0.1-xen.manifest.xml ramdiskURL=http://149.165.146.135:8773/services/Walrus/xeninitrd/initrd-2.6.27.21-0.1-xen.manifest.xml state=Extant ts=1320693195 ownerId=sharif keyName=ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCp13CbKJLtKG5prGiet/VHct36CXzcpBKVgYsh/lxIXWKuositayvvuKux+I5GZ9bFWzMF71xAjmFinmAT3FXFKMd54FebPKZ2kBPCRqtmxz2jT1SG4hy1g1eDPzVX+qt5w8metAs7W//BCaBvWpU5IBuKSNqxO5OUIjIKkw3xkSswRpqSzrUBAmQP7e4dzQvmhqIxq4ZWqcEIWsAik0fSODTipa+Z6DvVKe02f5OtdUsXzz7pIivZ3qRGQveI5SOTdgFPqG+VglMsPURLbFFVWW1l51gCmRUwTf9ClySshSpkpAtaOx/OApQoII/vJxgr/EdYPOu1QLkubS4XH6+Z sharif@eucalyptus ccnet={privateIp=10.0.4.4 publicIp=149.165.159.130 privateMac=D0:0D:42:BA:06:B1 vlan=10 networkIndex=4} ccvm={cores=1 mem=512 disk=5} ncHostIdx=24 serviceTag=http://i1:8775/axis2/services/EucalyptusNC userData= launchIndex=0 volumesSize=0 volumes={} groupNames={sharifnew }"
 
     print line
-    data = print_ccInstance_parser(line)
+    data = ccInstance_parser(line)
+    print json.dumps(data, sort_keys=False, indent=4)
 
+    line = "[Wed Nov  9 19:50:08 2011][008128][EUCADEBUG ] refresh_resources(): received data from node=i2 mem=24276/22740 disk=306400/305364 cores=8/6"
+    print line
+    data = refresh_resource_parser(line)
     print json.dumps(data, sort_keys=False, indent=4)
 
 if __name__ == "__main__":

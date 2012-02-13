bf0917c8a31dc8cea67b491c4603ec4d3e9c3335
Gregor von Laszewski <laszewski@gmail.com>
2012-02-13 07:06:05 -0500

    first preparation for a file read test

--- log-analyzer/futuregrid/eucalyptus/analyzer/fg-parser.py
+++ log-analyzer/futuregrid/eucalyptus/analyzer/fg-parser.py
@@ -44,12 +44,8 @@ def parse_type_and_date(line,data):
     return rest
 
 
-def ccInstance_parser(line):
+def ccInstance_parser(rest,data):
     """parses the line and returns a dict"""
-    data = {}
-
-    # parse_type_and_date(line,data,rest)
-    rest = parse_type_and_date (line, data)
 
     # replace print_ccInstance(): with linetype=print_ccInstance
     #rest = rest.replace("print_ccInstance():","linetype=print_ccInstance")
@@ -82,41 +78,44 @@ def ccInstance_parser(line):
 
     return data
 
-def refresh_resource_parser(line):
+def refresh_resource_parser(rest,data):
     #[Wed Nov  9 19:50:08 2011][008128][EUCADEBUG ] refresh_resources(): received data from node=i2 mem=24276/22740 disk=306400/305364 cores=8/6
-    data = {}
-    data["calltype"] = "refresh_resources" 
-    rest = parse_type_and_date(line,data)
-    rest = re.sub("refresh_resources\(\): received data from","",rest).strip()
+    if (rest.find("received") > -1):
+        rest = re.sub("received data from","",rest).strip()
     # node=i2 mem=24276/22740 disk=306400/305364 cores=8/6
-    m = re.search( r'node=(.*) mem=(.*)[/](.*) disk=(.*)/(.*) cores=(.*)/(.*)', rest, re.M|re.I)
-    data["node"] = m.group(1)
-    data["mem"] = m.group(2)
-    data["mem_max"] = m.group(3)
-    data["disk"] = m.group(4)
-    data["disk_max"] = m.group(5)
-    data["cores"] = m.group(6)
-    data["cores_max"] = m.group(7)
+        m = re.search( r'node=(.*) mem=(.*)[/](.*) disk=(.*)/(.*) cores=(.*)/(.*)', rest, re.M|re.I)
+        data["node"] = m.group(1)
+        data["mem"] = m.group(2)
+        data["mem_max"] = m.group(3)
+        data["disk"] = m.group(4)
+        data["disk_max"] = m.group(5)
+        data["cores"] = m.group(6)
+        data["cores_max"] = m.group(7)
+    else:
+        data["calltype"] = "ignore" 
+    return data
 
     return data
 
 
-def terminate_instances_param_parser(line):
-#[Thu Nov 10 10:14:37 2011][021251][EUCADEBUG ] TerminateInstances(): params: userId=(null), instIdsLen=1, firstInstId=i-417B07B2
-    data = {}
-    data["calltype"] = "terminate_instances_param" 
-    rest = parse_type_and_date(line,data)
-    rest = re.sub("TerminateInstances\(\): params:","",rest).strip()
-    # node=i2 mem=24276/22740 disk=306400/305364 cores=8/6
-    m = re.search( r'userId=(.*) instIdsLen=(.*) firstInstId=(.*)', rest, re.M|re.I)
-    userid = m.group(1)
-    if userid == "(null),":
-        data["userId"] = "null"
-    else:
-        data["userId"] = m.group(1)
-    data["instIdsLen"] = m.group(2)
-    data["firstInstId"] = m.group(3)
+def terminate_instances_param_parser(rest,data):
 
+    rest = rest.strip()
+    if rest.startswith("params"):
+#params: userId=(null), instIdsLen=1, firstInstId=i-417B07B2
+
+        rest = re.sub("params:","",rest).strip()
+    # node=i2 mem=24276/22740 disk=306400/305364 cores=8/6
+        m = re.search( r'userId=(.*) instIdsLen=(.*) firstInstId=(.*)', rest, re.M|re.I)
+        userid = m.group(1)
+        if userid == "(null),":
+            data["userId"] = "null"
+        else:
+            data["userId"] = m.group(1)
+        data["instIdsLen"] = m.group(2)
+        data["firstInstId"] = m.group(3)
+    else:
+        data["calltype"] = "ignore" 
     return data
 
 
@@ -124,23 +123,69 @@ def terminate_instances_param_parser(line):
 #####################################################################
 # MAIN
 #####################################################################
+def parse_test(f, line):
+    print "------------------------------------------------------------"
+    print "- parsetest: " + str(f)
+    print "------------------------------------------------------------"
+    print "INPUT>"
+    print line
+    data = {}
+    # parse_type_and_date(line,data,rest)
+    rest = parse_type_and_date (line, data)
+    print "REST>"
+    print rest
+    f(rest, data)
+    print "OUTPUT>"
+    print json.dumps(data, sort_keys=False, indent=4)
+
 def main():
 
-    line = "[Wed Nov  9 19:58:12 2011][008128][EUCADEBUG ] print_ccInstance(): refresh_instances():  instanceId=i-42BA06B1 reservationId=r-3D3306BC emiId=emi-0B951139 kernelId=eki-78EF12D2 ramdiskId=eri-5BB61255 emiURL=http://149.165.146.135:8773/services/Walrus/centos53/centos.5-3.x86-64.img.manifest.xml kernelURL=http://149.165.146.135:8773/services/Walrus/xenkernel/vmlinuz-2.6.27.21-0.1-xen.manifest.xml ramdiskURL=http://149.165.146.135:8773/services/Walrus/xeninitrd/initrd-2.6.27.21-0.1-xen.manifest.xml state=Extant ts=1320693195 ownerId=sharif keyName=ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCp13CbKJLtKG5prGiet/VHct36CXzcpBKVgYsh/lxIXWKuositayvvuKux+I5GZ9bFWzMF71xAjmFinmAT3FXFKMd54FebPKZ2kBPCRqtmxz2jT1SG4hy1g1eDPzVX+qt5w8metAs7W//BCaBvWpU5IBuKSNqxO5OUIjIKkw3xkSswRpqSzrUBAmQP7e4dzQvmhqIxq4ZWqcEIWsAik0fSODTipa+Z6DvVKe02f5OtdUsXzz7pIivZ3qRGQveI5SOTdgFPqG+VglMsPURLbFFVWW1l51gCmRUwTf9ClySshSpkpAtaOx/OApQoII/vJxgr/EdYPOu1QLkubS4XH6+Z sharif@eucalyptus ccnet={privateIp=10.0.4.4 publicIp=149.165.159.130 privateMac=D0:0D:42:BA:06:B1 vlan=10 networkIndex=4} ccvm={cores=1 mem=512 disk=5} ncHostIdx=24 serviceTag=http://i1:8775/axis2/services/EucalyptusNC userData= launchIndex=0 volumesSize=0 volumes={} groupNames={sharifnew }"
 
-    print line
-    data = ccInstance_parser(line)
-    print json.dumps(data, sort_keys=False, indent=4)
 
-    line = "[Wed Nov  9 19:50:08 2011][008128][EUCADEBUG ] refresh_resources(): received data from node=i2 mem=24276/22740 disk=306400/305364 cores=8/6"
-    print line
-    data = refresh_resource_parser(line)
-    print json.dumps(data, sort_keys=False, indent=4)
+    parse_test(ccInstance_parser, 
+               "[Wed Nov  9 19:58:12 2011][008128][EUCADEBUG ] print_ccInstance(): refresh_instances():  instanceId=i-42BA06B1 reservationId=r-3D3306BC emiId=emi-0B951139 kernelId=eki-78EF12D2 ramdiskId=eri-5BB61255 emiURL=http://149.165.146.135:8773/services/Walrus/centos53/centos.5-3.x86-64.img.manifest.xml kernelURL=http://149.165.146.135:8773/services/Walrus/xenkernel/vmlinuz-2.6.27.21-0.1-xen.manifest.xml ramdiskURL=http://149.165.146.135:8773/services/Walrus/xeninitrd/initrd-2.6.27.21-0.1-xen.manifest.xml state=Extant ts=1320693195 ownerId=sharif keyName=ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCp13CbKJLtKG5prGiet/VHct36CXzcpBKVgYsh/lxIXWKuositayvvuKux+I5GZ9bFWzMF71xAjmFinmAT3FXFKMd54FebPKZ2kBPCRqtmxz2jT1SG4hy1g1eDPzVX+qt5w8metAs7W//BCaBvWpU5IBuKSNqxO5OUIjIKkw3xkSswRpqSzrUBAmQP7e4dzQvmhqIxq4ZWqcEIWsAik0fSODTipa+Z6DvVKe02f5OtdUsXzz7pIivZ3qRGQveI5SOTdgFPqG+VglMsPURLbFFVWW1l51gCmRUwTf9ClySshSpkpAtaOx/OApQoII/vJxgr/EdYPOu1QLkubS4XH6+Z sharif@eucalyptus ccnet={privateIp=10.0.4.4 publicIp=149.165.159.130 privateMac=D0:0D:42:BA:06:B1 vlan=10 networkIndex=4} ccvm={cores=1 mem=512 disk=5} ncHostIdx=24 serviceTag=http://i1:8775/axis2/services/EucalyptusNC userData= launchIndex=0 volumesSize=0 volumes={} groupNames={sharifnew }")
 
-    line = "[Thu Nov 10 10:14:37 2011][021251][EUCADEBUG ] TerminateInstances(): params: userId=(null), instIdsLen=1, firstInstId=i-417B07B2"
-    print line
-    data = terminate_instances_param_parser(line)
-    print json.dumps(data, sort_keys=False, indent=4)
+
+    #
+    # RESOURCE PARSER
+    #
+
+
+    parse_test(refresh_resource_parser, 
+               "[Wed Nov  9 22:52:10 2011][008128][EUCAERROR ] refresh_resources(): bad return from ncDescribeResource(i23) (1)")
+    parse_test(refresh_resource_parser, 
+               "[Wed Nov  9 22:52:11 2011][008128][EUCADEBUG ] refresh_resources(): done")
+    parse_test(refresh_resource_parser, 
+               "[Wed Nov  9 22:52:19 2011][008128][EUCAINFO  ] refresh_resources(): called")
+    parse_test(refresh_resource_parser, 
+               "[Wed Nov  9 19:50:08 2011][008128][EUCADEBUG ] refresh_resources(): received data from node=i2 mem=24276/22740 disk=306400/305364 cores=8/6")
+
+    #
+    # TerminateInstance Parser
+    #
+    parse_test(terminate_instances_param_parser,
+               "[Thu Nov 10 10:14:37 2011][021251][EUCADEBUG ] TerminateInstances(): params: userId=(null), instIdsLen=1, firstInstId=i-417B07B2")
+
+    parse_test(terminate_instances_param_parser,
+               "[Thu Nov 10 13:04:16 2011][016124][EUCADEBUG ] TerminateInstances(): done.")
+
+    parse_test(terminate_instances_param_parser,
+               "[Thu Nov 10 13:04:16 2011][016168][EUCAINFO  ] TerminateInstances(): called")
+
+
+    #
+    # FILE TEST
+    #
+    # while <>
+    #   line = readline
+    #   data = {}
+    #   rest = parse_type_and_date (line, data)
+    #   if data["linetype"] == "?":
+    #      terminate_instances_param_parser(rest, data)
+    #   else if ...
+
+
+    #   
 
 if __name__ == "__main__":
     main()

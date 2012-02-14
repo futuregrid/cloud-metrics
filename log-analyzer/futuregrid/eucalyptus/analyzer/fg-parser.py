6fbf0d4333d7141f3944991714553f8388ae6db3
Gregor von Laszewski <laszewski@gmail.com>
2012-02-13 23:00:13 -0500

    added version checker

--- log-analyzer/futuregrid/eucalyptus/analyzer/fg-parser.py
+++ log-analyzer/futuregrid/eucalyptus/analyzer/fg-parser.py
@@ -53,10 +53,6 @@ def calculate_user_stats (instances,users):
         users[name][4] = float(users[name][1]) / float(users[name][0])
  
         
-
-        
-
-
 def convert_data_to_list(data,attribute):
     rest = data[attribute]
     rest = re.sub(" ","' , '", rest)
@@ -262,6 +258,14 @@ def parse_test(f, line):
     print json.dumps(data, sort_keys=False, indent=4)
 
 def main():
+
+
+
+    if sys.version_info < (2, 7):
+        print "ERROR: you must use python 2.7 or greater"
+        exit (1)
+    else:
+        print "Python version: " + str(sys.version_info)
 
     parse_test(ccInstance_parser, 
                "[Wed Nov  9 19:58:12 2011][008128][EUCADEBUG ] print_ccInstance(): refresh_instances():  instanceId=i-42BA06B1 reservationId=r-3D3306BC emiId=emi-0B951139 kernelId=eki-78EF12D2 ramdiskId=eri-5BB61255 emiURL=http://149.165.146.135:8773/services/Walrus/centos53/centos.5-3.x86-64.img.manifest.xml kernelURL=http://149.165.146.135:8773/services/Walrus/xenkernel/vmlinuz-2.6.27.21-0.1-xen.manifest.xml ramdiskURL=http://149.165.146.135:8773/services/Walrus/xeninitrd/initrd-2.6.27.21-0.1-xen.manifest.xml state=Extant ts=1320693195 ownerId=sharif keyName=ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCp13CbKJLtKG5prGiet/VHct36CXzcpBKVgYsh/lxIXWKuositayvvuKux+I5GZ9bFWzMF71xAjmFinmAT3FXFKMd54FebPKZ2kBPCRqtmxz2jT1SG4hy1g1eDPzVX+qt5w8metAs7W//BCaBvWpU5IBuKSNqxO5OUIjIKkw3xkSswRpqSzrUBAmQP7e4dzQvmhqIxq4ZWqcEIWsAik0fSODTipa+Z6DvVKe02f5OtdUsXzz7pIivZ3qRGQveI5SOTdgFPqG+VglMsPURLbFFVWW1l51gCmRUwTf9ClySshSpkpAtaOx/OApQoII/vJxgr/EdYPOu1QLkubS4XH6+Z sharif@eucalyptus ccnet={privateIp=10.0.4.4 publicIp=149.165.159.130 privateMac=D0:0D:42:BA:06:B1 vlan=10 networkIndex=4} ccvm={cores=1 mem=512 disk=5} ncHostIdx=24 serviceTag=http://i1:8775/axis2/services/EucalyptusNC userData= launchIndex=0 volumesSize=0 volumes={} groupNames={sharifnew }")

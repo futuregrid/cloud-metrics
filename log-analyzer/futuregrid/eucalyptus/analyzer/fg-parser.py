ea6f287219864931d023e74c157fcc853404af43
Gregor von Laszewski <laszewski@gmail.com>
2012-02-14 14:53:23 -0500

    created database structure

--- log-analyzer/futuregrid/eucalyptus/analyzer/fg-parser.py
+++ log-analyzer/futuregrid/eucalyptus/analyzer/fg-parser.py
@@ -13,10 +13,104 @@ from datetime import datetime
 
 users = {}
 instance = {}
+#
+# SQL
+#
+"""
+keynames = ()
+values = ()
+
+def getsql(key, values)
+    i = index(key,keynames)
+    return values[i]
+
+def get(key, values)
+    sqlkey = datakey(key)
+    return getsql(sqlkey,values)
+
+get(["tarce"]["teardown"]["start"]', values)
+
+value = a
+key = ["tarce"]["teardown"]["start"]'
+eval (instance key = value)
+
+instance["tarce"]["teardown"]["start"]' = value
+
+
+
+def sqlkey(string)
+    # '["tarce"]["teardown"]["start"]'
+    string = re.sub("][", "_", string)
+    string = re.sub("]", "", string)
+    string = re.sub("[", "", string)
+    string = re.sub('"', "", string)
+    string = re.sub("'", "", string)
+    return string
+
+def datakey(string)
+    # trace_teardown_start
+    string = re.sub("_",'"]["', string)
+    string = "['" + string + "']"
+    return string
+"""
+
+"""
+data
+        'trace': {
+            'teardown': {
+                'start': '2011-11-10 10:06:58', 
+                'stop': '2011-11-10 10:10:02'
+            }, 
+
+sql
+        trace_teardown_start: '2011-11-10 10:06:58', 
+        trace_teardown_start: '2011-11-10 10:10:02'
+
+data
+"groupNames": [
+            "sharifnew",             "sharifnewa"
+        ], 
+
+sql
+"groupNames": sharifnew sharifnewa
+        
+
+
+
+null
+"""
+def emptyData():
+return data
+
+
+def writetodb(data):
+    return
+
+
+def finddata (is, timestamp, user):
+    #    return object to pass to data_from_db
+    return
+    
+def fromdb(data_from_db):
+    data{}
+    return data
+
+    
 
 ######################################################################
 # GENERATE INSTANCE STATISTICS
 ######################################################################
+
+#instance_c = {"id": null }
+
+#def instance_cache(id):
+#    if id == instance_c["id"]:
+#        skip
+#    else:
+#        # witre instance_c[id] to db
+#        # instance_c[id] = get instance with id from db
+        
+    
 
 def minmax_time (a, b):
     if a < b:
@@ -37,6 +131,7 @@ def generate_instance_info (data):
 
         try:
             current = instance[id]
+            # if were wereto do a data base this line needs to be replaced
         except:
             current = data
             

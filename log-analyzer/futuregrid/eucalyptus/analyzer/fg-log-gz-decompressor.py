d1b8c1d342941eec34ed20d07cdc7a019d650946
Hyungro Lee <roehlee@gmail.com>
2012-02-17 04:26:01 -0500

    Log files decompressor(extractor) converted to python

--- log-analyzer/futuregrid/eucalyptus/analyzer/fg-log-gz-decompressor.py
+++ log-analyzer/futuregrid/eucalyptus/analyzer/fg-log-gz-decompressor.py
@@ -1,110 +1,91 @@
-#!/bin/bash
+'''
+fg.log.gz.decompressor.py
+=========================
 
-# fg.log.gz.decompressor.sh
-# =========================
-#
-# log decompressor from backup log files
-# This is basically to run 'tar'
-#
-# =========================
-# Last updated by 01/26/2012
-# Hyungro Lee (lee212 at indiana dot edu)
+This script is decompressing cc.log.tar.gz files under /var/log/eucalyptus/logbackup/YYYYMMDDhhmmss/
 
-source fg-log-analyzer-lib.sh 
+Usage
+-----
+fg.log.gz.decompressor.py [options]
 
-# Read from log-analyzer.cfg
-#
-# EUCA_LOGINPUT_DIR= ...
-# EUCA_LOGOUTPUT_DIR= ...
-#
-source log-analyzer.cfg
-#
+Options
+-------
+-s: start date
+-e: end date
+-o: output directory
 
-LIST=`ls $EUCA_LOGINPUT_DIR`
+*All options are required*
 
-#####################################################################
-# COMMON FUNCTIONS
-#####################################################################
+Example
+-------
+./fg.log.gz.decompressor.py -s 20120216 -e 20120216 -o /var/log/eucalyptus/BACKUP
 
-# This function checks if the parameter is an integer and returns 0 if it is an integer and 1 if not.
+Contact
+=========================
+Hyungro Lee (lee212 at indiana dot edu)
+'''
 
-function is_integer() {
-    [ "$1" -eq "$1" ] > /dev/null 2>&1
-    return $?
-}
+import argparse
+from datetime import datetime, time
+import sys
 
+def CheckDate(str):
+	'''Checks a date if it is in a valid format'''
+	try:
+		d = datetime.strptime(str, "%Y%m%d")
+		return True
+	except:
+		return False
 
-#####################################################################
-# ANALYSING THE ARGUMENTS
-#####################################################################
-while getopts "s:e:" opt;
-do
-	case $opt in
-	s) s_date=$OPTARG;;
-	e) e_date=$OPTARG;;
-	\?) echo invalid options $@;
-		echo "ex) $0 -s start date (20120115) -e end date (20120123)"
-		exit 1;;
-	esac
-done
+if __name__ == '__main__':
 
-if [ ! -z $s_date ] && [ ! -z $e_date ] 
-then
-	if ( ! is_integer $s_date ) || ( ! is_integer $e_date )
-	then
-		echo "$s_date, $e_date are not valid for date type (YYYYMMDD)."
-		exit 1;
-	fi
+	# Set variables
+	# -------------
+	input_dir="/var/log/eucalyptus/logbackup"
+	compressed_file="cclog.tar.gz"
 
-	s_year=`echo $s_date|cut -c -4`
-	s_month=`echo $s_date|cut -c 5-6`
-	s_day=`echo $s_date|cut -c 7-8`
-	e_year=`echo $e_date|cut -c -4`
-	e_month=`echo $e_date|cut -c 5-6`
-	e_day=`echo $e_date|cut -c 7-8`
-else
-	# Put yesterday
-	s_year=`date -d '1 day ago' +%Y`
-	s_month=`date -d '1 day ago' +%m`
-	s_day=`date -d '1 day ago' +%d`
-	e_year=$s_year
-	e_month=$s_month
-	e_day=$s_day
-fi
+	# Parse arguments
+	# ---------------
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-s", dest="s_date", required=True,
+		help="start date to begin decompression (type: YYYYMMDD)")
+	parser.add_argument("-e", dest="e_date", required=True,
+		help="end date to finish decompression (type: YYYYMMDD)")
+	parser.add_argument("-i", dest="input_dir", default=input_dir,
+		help="Absolute path where compressed .tar.gz files exist")
+	parser.add_argument("-o", dest="output_dir", required=True,
+		help="Absolute path where decompressed files to be saved")
+    
+	args = parser.parse_args()
+	#print args.s_date, args.e_date, args.input_dir, args.output_dir
 
-for i in $LIST
-do
-	year=`echo $i|cut -c -4`
-	month=`echo $i|cut -c 5-6`
-	day=`echo $i|cut -c 7-8`
-	hour=`echo $i|cut -c 9-10`
+	# Validate arguments
+	# ------------------
+	if not CheckDate(args.s_date) or not CheckDate(args.e_date):
+		print "input dates is(are) invalid!"
+		sys.exit(-1)
 
-	if ( ! is_integer $year ) || ( ! is_integer $month ) || ( ! is_integer $day ) || ( ! is_integer $hour )
-	then
-		echo no valid folder/file name "$i"
-		continue
-	fi
+	# Set vaariables
+	s_date = datetime.strptime(args.s_date, "%Y%m%d")
+	e_date = datetime.strptime(args.e_date, "%Y%m%d")
+	e_date = datetime.combine(e_date,time(23, 59, 59))
 
-	if [ "$year$month$day" -lt "$s_year$s_month$s_day" ]
-	then
-		#echo "Start date is $s_year/$s_month/$s_day, $year/$month/$day will be skipped"
-		continue
-	fi
-	if [ "$year$month$day" -gt "$e_year$e_month$e_day" ]
-	then
-		#echo "End date is $e_year/$e_month/$e_day, $year/$month/$day will be skipped"
-		continue
-	fi
+	# Read input directory
+	import os
+	list = os.listdir(input_dir)
+	for infile in list:
+		if not CheckDate(infile[0:8]):
+			continue;
+		dir_date = datetime.strptime(infile, "%Y%m%d%H%M")
+		if dir_date < s_date:
+			continue
+		if dir_date > e_date:
+			continue
+		#print s_date, e_date, dir_date
 
-	echo "[$i] [$year/$month/$day $hour]"
-
-	dest=$EUCA_LOGOUTPUT_DIR/$i
-	mkdir -p $dest
-	echo mkdir -p $dest
-
-	compressed_file=$EUCA_LOGINPUT_DIR/$i/cclog.tar.gz
-
-	tar xvzf $compressed_file -C $dest
-	echo tar xvzf $compressed_file -C $dest
-
-done
+		# Extract .tar.gz log files
+		import tarfile
+		print "Extracting... "+infile+"/"+compressed_file+" to "+args.output_dir+"/"+infile+"/..."
+		tar = tarfile.open(args.input_dir+"/"+infile+"/"+compressed_file, "r:gz")
+		tar.extractall(args.output_dir+"/"+infile)
+		

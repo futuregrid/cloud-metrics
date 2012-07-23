6345d3b54dd655197d2fb2edb2aa1d05c0583b14
lee212 <roehlee@gmail.com>
2012-07-23 12:34:05 -0400

    added openstack report

--- doc/source/generate-results.py
+++ doc/source/generate-results.py
@@ -168,6 +168,26 @@ class Results:
  
         content = content + (self.get_chart() % vars()) % vars()
 
+        # Data for OpenStack on India is only available after 06/01/2012
+        if self.start_date >= datetime.date(2012, 6, 01):
+            number += 1
+            metric = "count"
+            platform = "openstack"
+            nodename = "india"
+
+            main_title = content + self.get_content_sub_header(nodename, platform)
+     
+            src = "data/%(month)s/%(nodename)s/%(platform)s/user/%(metric)s/barhighcharts.html"
+            title = "Figure %(number)s. Total %(metric)s of VMs submitted per user for %(month_n_year)s on %(nodename)s"
+            content = main_title + self.get_chart() % vars()
+            content = content % vars()
+
+            number += 1
+            metric = "runtime"
+            title = "Figure %(number)s. Total %(metric)s (hour) of VMs submitted per user for %(month_n_year)s on %(nodename)s"
+
+            content = content + (self.get_chart() % vars()) % vars()
+
         # Data on Sierra is only available after 05/01/2012
         if self.start_date >= datetime.date(2012, 5, 01):
             number += 1
@@ -196,8 +216,8 @@ class Results:
      
             content = content + (self.get_chart() % vars()) % vars()
 
-        return content
 
+        return content
 
     def get_content_weekly(self):
         width = 800
@@ -234,6 +254,26 @@ class Results:
         title = "Figure %(number)s. Total VMs count per node cluster for %(start_date)s  ~ %(end_date)s on %(nodename)s"
  
         content = content + (self.get_chart() % vars()) % vars()
+
+        # Data for OpenStack on India is only available after 06/01/2012
+        if self.start_date >= datetime.date(2012, 6, 01):
+            number += 1
+            metric = "count"
+            platform = "openstack"
+            nodename = "india"
+
+            main_title = self.get_content_sub_header(nodename, platform)
+     
+            src = "data/%(end_date)s/%(nodename)s/%(platform)s/user/%(metric)s/barhighcharts.html"
+            title = "Figure %(number)s. Total %(metric)s of VMs submitted per user for %(start_date)s ~ %(end_date)s on %(nodename)s"
+            content = content + main_title + self.get_chart() % vars()
+            content = content % vars()
+
+            number += 1
+            metric = "runtime"
+            title = "Figure %(number)s. Total %(metric)s (hour) of VMs submitted per user for %(start_date)s ~ %(end_date)s on %(nodename)s"
+
+            content = content + (self.get_chart() % vars()) % vars()
 
         # Data on Sierra is only available after 05/01/2012
         if self.start_date >= datetime.date(2012, 5, 01):

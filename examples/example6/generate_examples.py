b4f42cdc86d734345e7b81d2db73261d2d10230b
lee212 <roehlee@gmail.com>
2012-07-27 13:13:31 -0400

    added reports for July

--- examples/example6/generate_examples.py
+++ examples/example6/generate_examples.py
@@ -1,7 +1,7 @@
 import datetime
 start_date = datetime.date(2011, 11, 01)
 start_date_for_weekly = start_date
-t_end_date = datetime.date(2012, 06, 12)
+t_end_date = datetime.date(2012, 07, 26)
 week = datetime.timedelta(weeks=1)
 
 template_file = ".template"

a9f5d4c38481e4eca06135c3419697e2058408f7
lee212 <roehlee@gmail.com>
2012-10-19 04:29:05 -0400

    quartely reports added

--- results/source/generate-results.py
+++ results/source/generate-results.py
@@ -15,7 +15,6 @@ class Results:
     week = datetime.timedelta(weeks=1)
     index_txt = None
     index_filename = name + docs_ext
-    month = start_date.timetuple()[1]
 
     docs_path = name + "/"
     indent = "\t"
@@ -24,6 +23,7 @@ class Results:
     def generate_index(self):
 
         index_txt = ""
+        lines = ""
 
         start_date = self.start_date
         end_date = self.t_end_date
@@ -32,22 +32,19 @@ class Results:
             if start_date > end_date:
                 break
 
-            current_month = start_date.timetuple()[1]
             #results.rst (index file)
-            content_filename = start_date.strftime("%Y-%m")
-            index_content = self.indent + self.docs_path + content_filename + self.newline
-            index_txt = index_content + index_txt
+            each_month = self.indent + self.docs_path + start_date.strftime("%Y-%m") + self.newline
+            index_txt = each_month + index_txt 
             year, month, day = start_date.timetuple()[:3]
+            if month % 3 == 0:
+                quarter = self.indent + self.docs_path + "Q" + str(self.get_quarter(month)) + self.newline
+                index_txt = quarter + index_txt
             new_month = month + 1
             start_date = datetime.date(year + (new_month / 13), (new_month % 12) or 12, day)
 
-        #EXCEPTION FOR ALL ENTRY
+        #EXCEPTION FOR this month, realtime, and ALL ENTRY
         index_txt = self.indent + self.docs_path + "thismonth" + self.newline + index_txt
-
-        #EXCEPTION FOR ALL ENTRY
         index_txt = self.indent + self.docs_path + "realtime" + self.newline + index_txt
-
-        #EXCEPTION FOR ALL ENTRY
         index_txt = self.indent + self.docs_path + "all" + self.newline + index_txt
 
         self.index_txt = self.get_index_header() + index_txt
@@ -91,6 +88,7 @@ class Results:
 
         monthly_report = self.get_content_monthly()
         weekly_report = ""
+        month = self.start_date.timetuple()[1]
 
         while (1):
             if self.start_date > self.t_end_date:
@@ -98,7 +96,7 @@ class Results:
  
             # If a next month comes, generate contents and clear variables for the next
             current_month = self.start_date.timetuple()[1]
-            if ((self.month + 1) % 12 or 12) == current_month:
+            if ((month + 1) % 12 or 12) == current_month:
                 contents = monthly_report + weekly_report
                 FGUtility.ensure_dir(content_filepath)
                 f = open (content_filepath, "w")
@@ -108,8 +106,17 @@ class Results:
                 # Clear variables
                 monthly_report = self.get_content_monthly()
                 weekly_report = ""
-                self.month = current_month
+                month = current_month
 
+            if current_month % 3 == 0:
+                contents = self.get_content_quarter()
+                content_filename = self.start_date.strftime("%Y-Q") + str(self.get_quarter(current_month))
+                content_filepath = self.docs_path + content_filename + self.docs_ext
+                FGUtility.ensure_dir(content_filepath)
+                f = open (content_filepath, "w")
+                f.write(contents)
+                f.close
+ 
             content_filename = self.start_date.strftime("%Y-%m")
             content_filepath = self.docs_path + content_filename + self.docs_ext
            
@@ -128,6 +135,229 @@ class Results:
         f = open (content_filepath, "w")
         f.write(contents)
         f.close
+
+    def get_content_quarter(self):
+        width = 800
+        height = 600
+        start_date = str(self.start_date)
+        end_date = str(self.end_date)
+        current_month = self.start_date.timetuple()[1]
+        month_n_year2 = "Q" + str(self.get_quarter(current_month))
+        month_n_year = month_n_year2 + self.start_date.strftime(" %Y")
+        month = self.start_date.strftime("%Y-") + month_n_year2
+
+        number = 1
+        metric = "count"
+        platform = "eucalyptus"
+        nodename = "india"
+
+        main_title = month_n_year + self.newline + \
+                "========================================" + self.newline
+
+        main_title += "Quarterly report (" + month_n_year2 + ")" + self.newline + \
+                "----------------------------------------" + self.newline
+
+        main_title += self.get_content_sub_header(nodename, platform)
+ 
+        src = "data/%(month)s/%(nodename)s/%(platform)s/user/%(metric)s/barhighcharts.html"
+        title = "Figure %(number)s. Total %(metric)s of VMs submitted per user for %(month_n_year)s on %(nodename)s"
+        content = main_title + self.get_chart() % vars()
+        content = content % vars()
+
+        number += 1
+        src = "data/%(month)s/%(nodename)s/%(platform)s/user/FGGoogleMotionChart.html"
+        content = content + (self.get_chart() % vars()) % vars()
+
+        number += 1
+        metric = "runtime"
+        title = "Figure %(number)s. Total %(metric)s (hour) of VMs submitted per user for %(month_n_year)s on %(nodename)s"
+        src = "data/%(month)s/%(nodename)s/%(platform)s/user/%(metric)s/barhighcharts.html"
+
+        content = content + (self.get_chart() % vars()) % vars()
+
+        number += 1
+        metric = "count"
+
+        src = "data/%(month)s/%(nodename)s/%(platform)s/%(metric)s/master-detailhighcharts.html"
+        title = "Figure %(number)s. Total %(metric)s of VMs submitted on %(nodename)s in %(month_n_year)s"
+ 
+        content = content + (self.get_chart() % vars()) % vars()
+
+        number += 1
+        metric = "runtime"
+
+        content = content + (self.get_chart() % vars()) % vars()
+
+        number += 1
+        metric = "ccvm_cores"
+
+        content = content + (self.get_chart() % vars()) % vars()
+
+        number += 1
+        metric = "ccvm_mem"
+
+        content = content + (self.get_chart() % vars()) % vars()
+
+        number += 1
+        metric = "ccvm_disk"
+
+        content = content + (self.get_chart() % vars()) % vars()
+
+        number += 1
+        metric = "count_node"
+
+        src = "data/%(month)s/%(nodename)s/%(platform)s/%(metric)s/columnhighcharts.html"
+        title = "Figure %(number)s. Total VMs count per node cluster for %(month_n_year)s on %(nodename)s"
+ 
+        content = content + (self.get_chart() % vars()) % vars()
+
+        # Data for OpenStack on India is only available after 06/01/2012
+        if self.start_date >= datetime.date(2012, 6, 01):
+            number += 1
+            metric = "count"
+            platform = "openstack"
+            nodename = "india"
+
+            main_title = content + self.get_content_sub_header(nodename, platform)
+     
+            src = "data/%(month)s/%(nodename)s/%(platform)s/user/%(metric)s/barhighcharts.html"
+            title = "Figure %(number)s. Total %(metric)s of VMs submitted per user for %(month_n_year)s on %(nodename)s"
+            content = main_title + self.get_chart() % vars()
+            content = content % vars()
+
+            number += 1
+            metric = "runtime"
+            title = "Figure %(number)s. Total %(metric)s (hour) of VMs submitted per user for %(month_n_year)s on %(nodename)s"
+
+            content = content + (self.get_chart() % vars()) % vars()
+
+        # Data on Sierra is only available after 05/01/2012
+        if self.start_date >= datetime.date(2012, 5, 01):
+            number += 1
+            metric = "count"
+            platform = "eucalyptus"
+            nodename = "sierra"
+
+            main_title = content + self.get_content_sub_header(nodename, platform)
+     
+            src = "data/%(month)s/%(nodename)s/%(platform)s/user/%(metric)s/barhighcharts.html"
+            title = "Figure %(number)s. Total %(metric)s of VMs submitted per user for %(month_n_year)s on %(nodename)s"
+            content = main_title + self.get_chart() % vars()
+            content = content % vars()
+
+            number += 1
+            metric = "runtime"
+            title = "Figure %(number)s. Total %(metric)s (hour) of VMs submitted per user for %(month_n_year)s on %(nodename)s"
+
+            content = content + (self.get_chart() % vars()) % vars()
+
+            number += 1
+            metric = "count"
+
+            src = "data/%(month)s/%(nodename)s/%(platform)s/%(metric)s/master-detailhighcharts.html"
+            title = "Figure %(number)s. Total %(metric)s of VMs submitted on %(nodename)s in %(month_n_year)s"
+     
+            content = content + (self.get_chart() % vars()) % vars()
+
+            number += 1
+            metric = "runtime"
+
+            content = content + (self.get_chart() % vars()) % vars()
+
+            number += 1
+            metric = "ccvm_cores"
+
+            content = content + (self.get_chart() % vars()) % vars()
+
+            number += 1
+            metric = "ccvm_mem"
+
+            content = content + (self.get_chart() % vars()) % vars()
+
+            number += 1
+            metric = "ccvm_disk"
+
+            content = content + (self.get_chart() % vars()) % vars()
+
+            number += 1
+            metric = "count_node"
+
+            src = "data/%(month)s/%(nodename)s/%(platform)s/%(metric)s/columnhighcharts.html"
+            title = "Figure %(number)s. Total VMs count per node cluster for %(month_n_year)s on %(nodename)s"
+     
+            content = content + (self.get_chart() % vars()) % vars()
+
+        number += 1
+        metric = "count"
+        platform = "nimbus"
+        nodename = "hotel"
+
+        main_title = content + self.get_content_sub_header(nodename, platform)
+ 
+        src = "data/%(month)s/%(nodename)s/%(platform)s/user/%(metric)s/barhighcharts.html"
+        title = "Figure %(number)s. Total %(metric)s of VMs submitted per user for %(month_n_year)s on %(nodename)s"
+        content = main_title + self.get_chart() % vars()
+        content = content % vars()
+
+        number += 1
+        metric = "runtime"
+        title = "Figure %(number)s. Total %(metric)s (hour) of VMs submitted per user for %(month_n_year)s on %(nodename)s"
+
+        content = content + (self.get_chart() % vars()) % vars()
+
+        number += 1
+        metric = "count"
+        nodename = "alamo"
+
+        main_title = content + self.get_content_sub_header(nodename, platform)
+ 
+        src = "data/%(month)s/%(nodename)s/%(platform)s/user/%(metric)s/barhighcharts.html"
+        title = "Figure %(number)s. Total %(metric)s of VMs submitted per user for %(month_n_year)s on %(nodename)s"
+        content = main_title + self.get_chart() % vars()
+        content = content % vars()
+
+        number += 1
+        metric = "runtime"
+        title = "Figure %(number)s. Total %(metric)s (hour) of VMs submitted per user for %(month_n_year)s on %(nodename)s"
+
+        content = content + (self.get_chart() % vars()) % vars()
+
+        number += 1
+        metric = "count"
+        nodename = "foxtrot"
+
+        main_title = content + self.get_content_sub_header(nodename, platform)
+ 
+        src = "data/%(month)s/%(nodename)s/%(platform)s/user/%(metric)s/barhighcharts.html"
+        title = "Figure %(number)s. Total %(metric)s of VMs submitted per user for %(month_n_year)s on %(nodename)s"
+        content = main_title + self.get_chart() % vars()
+        content = content % vars()
+
+        number += 1
+        metric = "runtime"
+        title = "Figure %(number)s. Total %(metric)s (hour) of VMs submitted per user for %(month_n_year)s on %(nodename)s"
+
+        content = content + (self.get_chart() % vars()) % vars()
+
+        number += 1
+        metric = "count"
+        nodename = "sierra"
+
+        main_title = content + self.get_content_sub_header(nodename, platform)
+ 
+        src = "data/%(month)s/%(nodename)s/%(platform)s/user/%(metric)s/barhighcharts.html"
+        title = "Figure %(number)s. Total %(metric)s of VMs submitted per user for %(month_n_year)s on %(nodename)s"
+        content = main_title + self.get_chart() % vars()
+        content = content % vars()
+
+        number += 1
+        metric = "runtime"
+        title = "Figure %(number)s. Total %(metric)s (hour) of VMs submitted per user for %(month_n_year)s on %(nodename)s"
+
+        content = content + (self.get_chart() % vars()) % vars()
+
+        return content
+
 
     def get_content_monthly(self):
         width = 800
@@ -533,6 +763,9 @@ class Results:
                 self.indent + "%(title)s" + self.newline
                 
         return res
+
+    def get_quarter(self, mon):
+        return (mon - 1)//3 + 1
 
 def main():
     result = Results()

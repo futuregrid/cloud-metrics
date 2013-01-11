860662edf7c5c1b74f6cf5a3d9cb073d20fc0d17
lee212 <hroe.lee@gmail.com>
2013-01-11 11:21:22 -0500

    edited waiting time to 990

--- rasterizefromfile.js
+++ rasterizefromfile.js
@@ -33,7 +33,7 @@ if (system.args.length < 3 || system.args.length > 5) {
             window.setTimeout(function () {
                 page.render(output);
                 phantom.exit();
-            }, 900);
+            }, 990);
         }
     });
 }

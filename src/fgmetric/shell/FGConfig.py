69134fbe99503133c1032ae23bafcfc022bc670e
lee212 <hroe.lee@gmail.com>
2013-04-01 16:53:44 -0400

    added yaml configuration class

---
+++ src/fgmetric/shell/FGConfig.py
@@ -0,0 +1,26 @@
+import yaml
+
+class FGConfig:
+    default_cfg = os.getenv("HOME") + "/.futuregrid/" 
+    default_yaml = "futuregrid.yaml"
+    default_yaml_path = default_cfg + default_yaml
+
+    def __init__(self):
+        self.yaml_path = self.default_yaml_path
+
+    def get_config(self):
+        f = open(self.yaml_path)
+        dataMap = yaml.safe_load(f)
+        f.close()
+        return dataMap
+
+    def set_config(self, dataMap):
+        f = open(self.yaml_path, "w")
+        yaml.deump(dataMap, f)
+        f.close()
+
+    def set_yaml_path(self, path):
+        self.yaml_path = path
+
+    def get_yaml_path(self):
+        return self.yaml_path

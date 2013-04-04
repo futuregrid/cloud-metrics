1b5eb3f8202990916ff3e3c8ab39aa1c3c5d3fce
lee212 <hroe.lee@gmail.com>
2013-04-04 17:09:46 -0400

    implementing function to parse nova-api.log file

--- src/fgmetric/novaapi_monitoring/log_monitoring.py
+++ src/fgmetric/novaapi_monitoring/log_monitoring.py
@@ -59,8 +59,35 @@ class log_monitoring:
             return
 
     def parse_log(self, line):
+        """Parse nova-api.log
+
+        Log template:
+        DATE LOG_TYPE API_NAME [REQUEST_ID USER_ID TENANT_ID] EVENT_MESSAGE
+
+        Value type:
+            DATE: YYYY-MM-DD HH:MM:SS
+            LOG_TYPE: DEBUG|INFO|AUDIT|WARNING|ERROR|CRITICAL (*ref: nova/openstack/common/log.py)
+            API_NAME: string with dot(.) (ex. nova.api.ec2)
+            REQUEST_ID: req-[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4} (ex.req-60eef213-ba15-4e59-8bc2-9b16049e11c1)
+            USER_ID: [0-9a-f]{32} (id in keystone db)
+            TENANT_ID: [0-9a-f]{32} (id in keystone db)
+            EVENT_MESSAGE: msg
+
+        Example message:
+        2013-03-29 06:38:11 DEBUG nova.api.ec2 [req-77d26d51-0049-487e-a2af-a6e4fa79c056 e25c4264dcba4569a0c0d2f3f6a8919c 461884eef90047fbb4eb9ec92f22a1e3] action: DescribeAddresses from (pid=25893) __call__ /usr/lib/python2.7/dist-packages/nova /api/ec2/__init__.py:435
+        """
         try:
-            re.compile
+            regex = re.compile("(\d{2,4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2})\s([^\s]*)\s([^\s]*)\s\[([^\s]*)\s([^\s]*)\s([^\s]*)\]\s([^$]*)")
+            r = regex.search(line)
+            log = {}
+            log["date"] = r.group(1)
+            log["log_type"] = r.group(2)
+            log["api_name"] = r.group(3)
+            log["request_id"] = r.group(4)
+            log["user_id"] = r.group(5)
+            log["tenant_id"] = r.group(6)
+            log["event_message"] = r.group(7)
+
 
     def _test(self):
         self.log.setLevel(logging.DEBUG)
